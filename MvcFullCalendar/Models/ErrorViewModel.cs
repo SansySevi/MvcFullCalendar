408087dca9e1058967e2fe796d5747711916c870
SansySevi <ssevilleja@hotmail.es>
2023-04-09 20:42:57 +0200

    Add project files.

---
+++ MvcFullCalendar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcFullCalendar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
